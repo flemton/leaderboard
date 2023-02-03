7c824e9513c803b561dff698ea9e36bf71f015de
Alhassan Osman <oalhassan847@gmail.com>
2023-02-03 12:29:18 +0000

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css';
 import addScore from './modules/addScore';
-import getScores from './modules/getScores';
+import getScores from './modules/getScores.js';
 
 getScores();
 
-const submitBtn  = document.querySelector('#submit');
+const submitBtn = document.querySelector('#submit');
 submitBtn.addEventListener('click', addScore);
 
 const refresh = document.querySelector('#refresh');
--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -1,20 +1,20 @@
 const scoreUl = document.querySelector('.scores');
 
 async function getScores() {
-    let res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IqNrWE04pgaXdxjtzDh9/scores/')
-    let resj = await res.text();
-    const scores = JSON.parse(resj)['result'];
-    let leaders = '';
-    let i = 1;
-    scores.sort(function(a, b) {return b.score - a.score});
-    scores.forEach(score => {
-        leaders +=
-        `<li> 
+  const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IqNrWE04pgaXdxjtzDh9/scores/');
+  const resj = await res.text();
+  const scores = JSON.parse(resj).result;
+  let leaders = '';
+  let i = 1;
+  scores.sort((a, b) => b.score - a.score);
+  scores.forEach((score) => {
+    leaders
+        += `<li> 
         ${i}. ${score.user}: ${score.score}
-        </li>`
-        i += 1;
-    });
-    scoreUl.innerHTML = leaders
+        </li>`;
+    i += 1;
+  });
+  scoreUl.innerHTML = leaders;
 }
 
 export default getScores;
